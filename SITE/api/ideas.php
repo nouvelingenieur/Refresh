ff92e49a5c174477f1f89c91d1c36b5ef94fde70
asimov4 <asimov4@gmail.com>
2012-02-25 11:43:11 -0800

    API: added category.ph and search option on ideas.php

--- SITE/api/ideas.php
+++ SITE/api/ideas.php
@@ -12,7 +12,7 @@ Input
 integer n : number of ideas per page 
 integer p : page for pagination purposes 
 string q : search query
-integer category : id of the category to search (if unspecified, then all categories)
+integer c : id of the category to search (if unspecified, then all categories)
 
 Output
 
@@ -30,14 +30,35 @@ header('Content-type: application/json');
 
 include_once("./mysql_connect.php");
 
+/* INPUT */
+$n = set_value('n',5);
+$p = set_value('p',1);
+$q = set_value('q','');
+$c = set_value('c',0);
 
-if (!isset($_GET['n'])) {
-	$n = 5;
-} else {
-	$n = $_GET['n'];
+$WHERE_array = array();
+
+// q: Search query
+if ($q!='') {
+$WHERE_array[] = 'title LIKE "%'.$q.'%" OR text LIKE "%'.$q.'%" OR possibly_name LIKE "%'.$q.'%"';
 }
 
+// c: Category
+if ($c!=0) {
+$WHERE_array[] = 'category = '.$c;
+}
 
+$WHERE = '';
+if (count($WHERE_array) > 0) {
+	$WHERE = 'WHERE ';
+	$sep = '';
+	foreach ($WHERE_array as $contraint) {
+		$WHERE .= $sep.'('.$contraint.')';
+		$sep = ' AND ';
+	}
+}
+
+/* OUTPUT */
 $sql = "SELECT 
 	thread_id as IDEA_ID, 
 	category as IDEA_CATEOGRY_ID, 
@@ -46,6 +67,7 @@ $sql = "SELECT
 	date as IDEA_DATE, 
 	possibly_name as IDEA_AUTHOR 
 FROM thread
+".$WHERE."
 LIMIT ".$n;
 
 $result = $dbh->query($sql);
@@ -53,9 +75,7 @@ $result = $dbh->query($sql);
 $array = array();
 
 while( $row = $result->fetch(PDO::FETCH_ASSOC) ) {
-
 $array[] = $row;
-
 }
 
 
--- SITE/api/mysql_connect.php
+++ SITE/api/mysql_connect.php
@@ -3,4 +3,12 @@ include_once("../config.php");
 
 $dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME, DB_USER, DB_PASSWORD);
 
+function set_value($parameter,$default) {
+	if (!isset($_GET[$parameter])) {
+		return $default;
+	} else {
+		return $_GET[$parameter];
+	}
+}
+
 ?>
